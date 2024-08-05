f88241266a7248bc61876aefbd381a5308c45bd7
Marie Foxall <66482428+mariefoxall@users.noreply.github.com>
2024-08-05 08:28:11 -0400

    Revert "set it all up baby"
    This reverts commit 187af3d8f9bbba2d84cbfb746c4710c118027735.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
+import entretempsLogo from "./assets/entretemps-logo.png";
 import "./App.css";
-import Portfolio from "./Portfolio/Portfolio";
+import Menu from "./Menu/Menu";
+import Exhibitions from "./Exhibitions/Exhibitions";
 import Contact from "./Contact/Contact";
 import Visit from "./Visit/Visit";
 import Shop from "./Shop/Shop";
 import Home from "./Home/Home";
-import About from "./About/About";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <Router>
+      <header>
+        <button className="home-button">
+          <Link to="/">
+            <img src={entretempsLogo} className="logo" alt="Entretemps logo" />
+          </Link>
+        </button>
+        <Menu />
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/exhibitions" element={<Exhibitions />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/visit" element={<Visit />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
