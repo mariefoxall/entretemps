2cf9d284c4692a3c5642a26e1e9c6cb7cd6db4c3
Marie Foxall <66482428+mariefoxall@users.noreply.github.com>
2024-12-09 15:46:20 -0500

    add some yo doggy assets

--- src/Exhibitions/Exhibitions.jsx
+++ src/Exhibitions/Exhibitions.jsx
@@ -4,6 +4,9 @@ import lampAndMaze from "../assets/chainmail-lamp-bead-maze.jpg";
 import overall from "../assets/overall.jpg";
 import chair from "../assets/tall-chair.jpg";
 import doggyPoster from "../assets/yo-doggy-poster.png";
+import dogAndKong from "../assets/dog-kong.jpg";
+import captive from "../assets/captive-collar-stand.jpg";
+import overallYoDoggy from "../assets/yo-doggy-right-side.jpg";
 // import lamp from "../assets/entretemps-lamp.jpg";
 // import key from "../assets/entretemps-key.jpg";
 // import beadMaze from "../assets/bead-maze-close.jpg";
@@ -15,13 +18,28 @@ function Exhibitions() {
   return (
     <div className="exhibitions-page">
       <div className="exhibition-section">
-        <h2>Upcoming Exhibitions:</h2>
+        <h2>Current Exhibition:</h2>
         <h3>YO DOGGY!</h3>
 
         <img
           className="gallery-image-vert"
           src={doggyPoster}
           alt="Poster for upcoming exhibition YO DOGGY!"
+        />
+        <img
+          className="gallery-image"
+          src={overallYoDoggy}
+          alt="Wide shot of art gallery with multicolored sculptures all over"
+        />
+        <img
+          className="gallery-image-vert"
+          src={dogAndKong}
+          alt="Screenprint of dog and ceramic kong-shaped lamp"
+        />
+        <img
+          className="gallery-image-vert"
+          src={captive}
+          alt="Bronze and suede ornate dog collar on a brass and wooden stand"
         />
       </div>{" "}
       <div className="exhibition-section">
