c2a68ff6ca7ed60da073c15165c898b0a0a523a7
Marie Foxall <66482428+mariefoxall@users.noreply.github.com>
2025-06-05 14:22:13 -0400

    update exhibitions page

--- src/Exhibitions/Exhibitions.jsx
+++ src/Exhibitions/Exhibitions.jsx
@@ -6,56 +6,78 @@ import waitingPoster from "../assets/english_purgatory.jpg";
 // import beadMaze from "../assets/bead-maze-close.jpg";
 import princesseCowboy from "../assets/princesse-et-cowboy.jpg";
 import monDepOpenCall from "../assets/mon-depanneur-open-call-eng.jpg";
+import BPOB from "../assets/BlueprintsOfBelongingPoster.jpg";
 import "./Exhibitions.css";
 
 function Exhibitions() {
   return (
     <div className="exhibitions-page">
-      <h2>Upcoming Exhibition:</h2>
-      <div className="exhibition-section">
-        <h3>MON DÉPANNEUR</h3>
-        <p>Applications open until March 31</p>
-        <Link to="/contact">
+      <div className="upcoming">
+        <h2>Upcoming Exhibitions:</h2>
+        <div className="exhibition-section">
+          <div className="exhibition-header">
+            <h3>BLUEPRINTS OF BELONGING</h3>
+            <p>Lawrence Fafard</p>
+          </div>
+          <img
+            className="gallery-image-vert"
+            src={BPOB}
+            alt="Blueprints of Belonging photo exhibition poster"
+          />{" "}
+        </div>
+        <div className="exhibition-section">
+          <div className="exhibition-header">
+            <h3>MON DÉPANNEUR</h3>{" "}
+          </div>
           <img
             className="gallery-image-vert"
             src={monDepOpenCall}
             alt="Poster calling for submissions for upcoming exhibition Mon Dépanneur"
           />{" "}
-        </Link>
+        </div>
       </div>
-      <h2>Previous Exhibitions:</h2>
-      <div className="exhibition-section">
-        <h3>PRINCESSE & COWBOY</h3>
-        <p>Loup Gauvin-Dufresne & Laurence Savard-Gagnon</p>
-        <Link to="/exhibitions/princesse-et-cowboy">
-          <img
-            className="gallery-image-vert"
-            src={princesseCowboy}
-            alt="Poster for upcoming exhibition Princesse et Cowboy"
-          />{" "}
-        </Link>
-      </div>
-      <div className="exhibition-section">
-        <h3>YO DOGGY!</h3>
-        <Link to="/exhibitions/yo-doggy">
-          <img
-            className="gallery-image-vert"
-            src={doggyPoster}
-            alt="Poster for previous exhibition YO DOGGY!"
-          />{" "}
-        </Link>
+      <div className="previous">
+        <h2>Previous Exhibitions:</h2>
+        <div className="exhibition-section">
+          {" "}
+          <div className="exhibition-header">
+            <h3>PRINCESSE & COWBOY</h3>
+            <p>Loup Gauvin-Dufresne & Laurence Savard-Gagnon</p>
+          </div>
+          <Link to="/exhibitions/princesse-et-cowboy">
+            <img
+              className="gallery-image-vert"
+              src={princesseCowboy}
+              alt="Poster for upcoming exhibition Princesse et Cowboy"
+            />{" "}
+          </Link>
+        </div>
+        <div className="exhibition-section">
+          <div className="exhibition-header">
+            <h3>YO DOGGY!</h3>
+          </div>
+          <Link to="/exhibitions/yo-doggy">
+            <img
+              className="gallery-image-vert"
+              src={doggyPoster}
+              alt="Poster for previous exhibition YO DOGGY!"
+            />{" "}
+          </Link>
+        </div>{" "}
+        <div className="exhibition-section">
+          <div className="exhibition-header">
+            <h3>Purgatory is a Waiting Room</h3>
+            <p>Etty Anderson & Marie Foxall</p>
+          </div>
+          <Link to="/exhibitions/purgatory-is-a-waiting-room">
+            <img
+              className="gallery-image-vert"
+              src={waitingPoster}
+              alt="Poster for previous exhibition Purgatory is a Waiting Room"
+            />{" "}
+          </Link>
+        </div>
       </div>{" "}
-      <div className="exhibition-section">
-        <h3>Purgatory is a Waiting Room</h3>
-        <p>Etty Anderson & Marie Foxall</p>
-        <Link to="/exhibitions/purgatory-is-a-waiting-room">
-          <img
-            className="gallery-image-vert"
-            src={waitingPoster}
-            alt="Poster for previous exhibition Purgatory is a Waiting Room"
-          />{" "}
-        </Link>
-      </div>
     </div>
   );
 }
