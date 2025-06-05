811e556c728ab47c9ef66b83947299ae9b2f6ec0
Marie Foxall <66482428+mariefoxall@users.noreply.github.com>
2025-06-05 14:51:28 -0400

    update contact

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,14 +1,16 @@
 import "./Contact.css";
-import monDepOpenCall from "../assets/mon-depanneur-open-call-eng.jpg";
+// import monDepOpenCall from "../assets/mon-depanneur-open-call-eng.jpg";
+// import logo from "../assets/entretemps-logo.png";
+import pcOutside from "../assets/princesse-cowboy-outside.jpg";
 
 export default function Contact() {
   return (
     <div className="contact-page">
       <h2>Say bonjour/hi</h2>
       <div className="contact-section"></div>{" "}
-      <h3 className="spacing highlight">
+      {/* <h3 className="spacing highlight">
         Submissions are open for our upcoming show, <br />
-        Mon Dépanneur, opening May 8!
+        Mon Dépanneur, opening July 10!
       </h3>
       <p className="spacing">Submission deadline: March 31</p>
       <img
@@ -45,15 +47,27 @@ export default function Contact() {
         - OR -
         <br />- Sketches of a work you're proposing with supporting images of
         your previous work
-      </p>
-      {/* <h3>Do you have a burning question?</h3>
+      </p> */}
+      <h3>Do you have a burning question?</h3>
       <h3>Want to submit your work for consideration?</h3>
       <p className="email-address">
         Give us a shout at{" "}
         <a className="email-link" href="mailto:bonjour.hi@entretemps.ca">
           bonjour.hi@entretemps.ca
         </a>
-      </p> */}
+      </p>
+      {/* <div className="logo-container">
+        <img
+          className="entretemps-logo-spin"
+          src={logo}
+          alt="Entretemps gallery logo in an hourglass shape"
+        />{" "}
+      </div> */}
+      <img
+        className="gallery-front"
+        src={pcOutside}
+        alt="front window of Entretemps gallery "
+      />{" "}
     </div>
   );
 }
