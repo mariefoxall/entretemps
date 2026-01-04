1933a4be8af16503cfdb3f254b71b1fa3d796ffc
“Marie <marie@wastedeffort.ca>
2026-01-04 07:16:29 -0500

    update upcoming

--- src/Exhibitions/Exhibitions.jsx
+++ src/Exhibitions/Exhibitions.jsx
@@ -5,6 +5,8 @@ import calissePoster from "../assets/calisse-de-goblet-poster.jpg";
 import princesseCowboy from "../assets/princesse-et-cowboy.jpg";
 import monDepPoster from "../assets/dep-poster-letter-web.jpg";
 import BPOB from "../assets/BlueprintsOfBelongingPoster.jpg";
+import call from "../assets/jardin-secret/jardin-secret-call-submissions.jpg";
+import appel from "../assets/jardin-secret/jardin-secret-appel-dossiers.jpg";
 import "./Exhibitions.css";
 
 function Exhibitions() {
@@ -23,10 +25,29 @@ function Exhibitions() {
           />{" "}
         </div>
       </div> */}
-      {/* <div className="upcoming">
+      <div className="upcoming">
         <h2>Expositions à venir / Upcoming Exhibitions:</h2>
-       
-      </div> */}
+        <div className="open-call">
+          <div>
+            <Link to="https://forms.gle/8xXYR12jPh1bhE1D6">
+              <img
+                className="open-call-poster"
+                src={appel}
+                alt="Poster for open call for Jardin Secret Exhibition with link to application form in French"
+              />
+            </Link>
+          </div>
+          <div>
+            <Link to="https://forms.gle/PAeWjrwNRnXGGGG37">
+              <img
+                className="open-call-poster"
+                src={call}
+                alt="Poster for open call for Jardin Secret Exhibition with link to application form in English"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
       <div className="previous">
         <h2>
           Expositions passées /<br /> Previous Exhibitions:
@@ -34,9 +55,6 @@ function Exhibitions() {
         <p>Click the poster for images from that show!</p>
         <div className="previous-posters">
           <div className="exhibition-section">
-            {/* <div className="exhibition-header">
-              <h3>CALISSE DE GOBLET</h3>{" "}
-            </div> */}
             <Link to="/exhibitions/calisse-de-goblet">
               <img
                 className="poster-image"
@@ -46,9 +64,6 @@ function Exhibitions() {
             </Link>
           </div>{" "}
           <div className="exhibition-section">
-            {/* <div className="exhibition-header">
-              <h3>MON DÉPANNEUR</h3>{" "}
-            </div> */}
             <Link to="/exhibitions/mon-depanneur">
               <img
                 className="poster-image"
@@ -58,10 +73,6 @@ function Exhibitions() {
             </Link>
           </div>{" "}
           <div className="exhibition-section">
-            {/* <div className="exhibition-header">
-              <h3>BLUEPRINTS OF BELONGING</h3>
-              <p>Lawrence Fafard</p>
-            </div> */}
             <Link to="/exhibitions/blueprints-of-belonging">
               <img
                 className="poster-image"
@@ -72,10 +83,6 @@ function Exhibitions() {
           </div>
           <div className="exhibition-section">
             {" "}
-            {/* <div className="exhibition-header">
-              <h3>PRINCESSE & COWBOY</h3>
-              <p>Loup Gauvin-Dufresne & Laurence Savard-Gagnon</p>
-            </div> */}
             <Link to="/exhibitions/princesse-et-cowboy">
               <img
                 className="poster-image"
@@ -85,9 +92,6 @@ function Exhibitions() {
             </Link>
           </div>
           <div className="exhibition-section">
-            {/* <div className="exhibition-header">
-              <h3>YO DOGGY!</h3>
-            </div> */}
             <Link to="/exhibitions/yo-doggy">
               <img
                 className="poster-image"
@@ -97,10 +101,6 @@ function Exhibitions() {
             </Link>
           </div>{" "}
           <div className="exhibition-section">
-            {/* <div className="exhibition-header">
-              <h3>Purgatory is a Waiting Room</h3>
-              <p>Etty Anderson & Marie Foxall</p>
-            </div> */}
             <Link to="/exhibitions/purgatory-is-a-waiting-room">
               <img
                 className="poster-image"
